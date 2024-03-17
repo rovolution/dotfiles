aaf693946be95a439479d514cba1d930832ba295
Rohit Kalkur <rohit.kalkur@gmail.com>
2024-03-17 16:46:59 -0400

    adds zoom

--- brew.sh
+++ brew.sh
@@ -49,6 +49,7 @@ brew install spotify-notifications
 brew install slack
 brew install telegram
 brew install --cask whatsapp
+brew install --cask zoom
 brew install flux
 brew install iterm2
 brew install flycut
